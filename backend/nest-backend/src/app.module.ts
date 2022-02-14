9e5e472c0317cbfd5fc4698b47e97e55df04dd16
Winnie Thembelihle Nhlapo <winnie.nhlapo@dadevs.co.za>
2022-02-14 09:12:02 +0200

    database sequalize setup done

--- backend/nest-backend/src/app.module.ts
+++ backend/nest-backend/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-
-//typeorm module
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database module/database.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
 
   ],
 
