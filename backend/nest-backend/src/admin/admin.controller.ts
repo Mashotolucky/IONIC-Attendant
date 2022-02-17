ac37eea5f77a6b69c09ff5a1db5812bb2c39a36c
Vhutshilo  Khomola <vhutshilo.khomola@dadevs.co.za>
2022-02-17 16:53:47 +0200

    admin setup complete

--- backend/nest-backend/src/admin/admin.controller.ts
+++ backend/nest-backend/src/admin/admin.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AdminDto } from './dto/admin.dto';
+import { AdminService} from './admin.service'
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+    constructor (private adminService:AdminService){}
+    @Post('signup')
+    async signUp(@Body() admin: AdminDto) {
+        return await this.adminService.create(admin);
+    }
+
+}
--- backend/nest-backend/src/admin/admin.service.ts
+++ backend/nest-backend/src/admin/admin.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { AdminDto } from './dto/admin.dto';
+import { ADMIN_REPOSITORY } from '../constant';
+import { Admin} from './admin.entity';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+
+    constructor(@Inject(ADMIN_REPOSITORY) private readonly adminRepository: typeof Admin) { }
+
+    async create(admin: AdminDto): Promise<Admin> {
+        return await this.adminRepository.create<Admin>(admin);
+    }
+    async findOneByEmail(email: string): Promise<Admin> {
+        return await this.adminRepository.findOne<Admin>({ where: { email } });
+    }
+
+    async findOneById(id: number): Promise<Admin> {
+        return await this.adminRepository.findOne<Admin>({ where: { id } });
+    }
+}
