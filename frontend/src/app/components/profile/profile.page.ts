99460a3a7835c59f24a6ee0e91c3568f081e0057
acamia <lindotjatji@gmail.com>
2022-02-18 16:52:51 +0200

    edit profile

---
+++ frontend/src/app/components/profile/profile.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  url="";
+  onselectFile(e){
+    if(e.target.files){
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload=(event:any)=>{
+        this.url=event.target.result;
+      }
+    }
+  }
+
+
+}
