3959ffbc80977ea450506b345facd3b190845180
Vhutshilo  Khomola <vhutshilo.khomola@dadevs.co.za>
2022-02-21 14:23:07 +0200

    changes

--- backend/nest-backend/src/app.module.ts
+++ backend/nest-backend/src/app.module.ts
@@ -3,22 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database module/database.module';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { AttendanceModule } from './attendance/attendance.module';
-import { AdminModule } from './admin/admin.module';
 
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    DatabaseModule,
-    UsersModule,
-    AuthModule,
-    AttendanceModule,
-    AdminModule,
+  
 
   ],
 
--- backend/nest-backend/src/users/dto/user.dto.ts
+++
@@ -1,7 +0,0 @@
-export class UserDto {
-    readonly firstname: string;
-    readonly lastname: string;
-    readonly email: string;
-    readonly employeenumber: string;
-    readonly password: string;
-}
